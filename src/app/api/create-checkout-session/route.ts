2baa488249cd55c584cc44f88fb7ad4ff5da63ee
Stefan Andjelic <stefanandjelic8@gmail.com>
2025-02-08 01:55:39 -0500

    updated stripe

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,10 +1,6 @@
 import type { CartItem } from '@/types'
 import { NextResponse } from 'next/server'
-import Stripe from 'stripe'
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-01-27.acacia',
-})
+import { stripe } from '@/lib/stripe'
 
 export async function POST(req: Request) {
   try {
